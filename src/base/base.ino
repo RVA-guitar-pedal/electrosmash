9f92c9515c8cf93128b9b4e80c2096bf9231dc2e
Will O'Donovan <wjodon@gmail.com>
2016-06-18 13:33:10 -0400

    ok_octaver, noise_gate_waves, base

---
+++ src/base/base.ino
@@ -0,0 +1,61 @@
+// Licensed under a Creative Commons Attribution 3.0 Unported License.
+// Based on rcarduino.blogspot.com previous work.
+// www.electrosmash.com/pedalshield
+ 
+int in_ADC0, in_ADC1;  //variables for 2 ADCs values (ADC0, ADC1)
+int POT0, POT1, POT2, out_DAC0, out_DAC1; //variables for 3 pots (ADC8, ADC9, ADC10)
+const int LED = 3;
+const int FOOTSWITCH = 7; 
+const int TOGGLE = 2;
+
+void setup() {
+
+  //turn on the timer clock in the power management controller
+  pmc_set_writeprotect(false);
+  pmc_enable_periph_clk(ID_TC4);
+ 
+  //we want wavesel 01 with RC 
+  TC_Configure(TC1,1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK2);
+  TC_SetRC(TC1, 1, 238); // sets <> 44.1 Khz interrupt rate
+  TC_Start(TC1, 1);
+
+  // enable timer interrupts on the timer
+  TC1->TC_CHANNEL[1].TC_IER=TC_IER_CPCS;
+  TC1->TC_CHANNEL[1].TC_IDR=~TC_IER_CPCS;
+ 
+  //Enable the interrupt in the nested vector interrupt controller 
+  //TC4_IRQn where 4 is the timer number * timer channels (3) + the channel number 
+  //(=(1*3)+1) for timer1 channel1 
+  NVIC_EnableIRQ(TC4_IRQn);
+ 
+  //ADC Configuration
+  ADC->ADC_MR |= 0x80;   // DAC in free running mode.
+  ADC->ADC_CR=2;         // Starts ADC conversion.
+  ADC->ADC_CHER=0x1CC0;  // Enable ADC channels 0,1,8,9 and 10  
+ 
+  //DAC Configuration
+  analogWrite(DAC0,0);  // Enables DAC0
+  analogWrite(DAC1,0);  // Enables DAC0
+ 
+  //pedalSHIELD pin configuration
+  pinMode(LED, OUTPUT);  
+  pinMode(FOOTSWITCH, INPUT_PULLUP);      
+  pinMode(TOGGLE, INPUT_PULLUP);     
+
+}
+
+void loop() {
+
+  //Read the ADCs
+  while((ADC->ADC_ISR & 0x1CC0)!=0x1CC0);// wait for ADC 0,1,8,9,10 conversion complete.
+  in_ADC0=ADC->ADC_CDR[7];               // read data from ADC0
+  in_ADC1=ADC->ADC_CDR[6];               // read data from ADC1  
+  POT0=ADC->ADC_CDR[10];                 // read data from ADC8        
+  POT1=ADC->ADC_CDR[11];                 // read data from ADC9   
+  POT2=ADC->ADC_CDR[12];                 // read data from ADC10     
+
+  //Turn on the LED if the effect is ON.
+  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH); 
+     else  digitalWrite(LED, LOW);
+
+}
